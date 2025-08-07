d50b16cd7701fa7d16025d811063cd94886bbd52
Juan Andres Piñero <juanchipinero@MacBook-Air-de-Juan-2.local>
2025-08-07 12:32:19 -0400

    Modifique clase Obra

---
+++ obra.py
@@ -0,0 +1,22 @@
+from artista import Artista
+
+#Esta es la clase de las obras
+class Obra:
+    def __init__(self, id_obra, titulo, artista, tipo, fecha, imagen_url):
+        self.id_obra = id_obra
+        self.titulo = titulo
+        self.artista = artista  
+        self.tipo = tipo
+        self.fecha = fecha
+        self.imagen_url = imagen_url
+
+    def mostrar_info_basica(self):
+        print(f"ID: {self.id_obra} | Título: {self.titulo} | Artista: {self.artista.nombre}")
+
+    def mostrar_info_detallada(self):
+        print("\n----- DETALLE DE OBRA -----")
+        print(f"Título: {self.titulo}")
+        print(f"Tipo: {self.tipo}")
+        print(f"Año de creación: {self.fecha}")
+        self.artista.mostrar_info()
+        print(f"Imagen URL: {self.imagen_url}")
